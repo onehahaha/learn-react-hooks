d290947d4a94cbfe23f08c9e2e6931faf9f40aad
xiaqixiang <xiaqixiang00@163.com>
2020-02-11 22:14:00 +0800

    轮播组件搭建

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import { GlobalStyle } from './style';
 import { HashRouter} from 'react-router-dom';
 import { renderRoutes } from 'react-router-config'
 import routes from './routes/index'
+import store from './store/index';
+import { Provider } from 'react-redux'
 
 function App() {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      {renderRoutes(routes)}
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   );
 }
 
---
+++ src/components/slider/index.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import 'swiper/css/swiper.css';
+import { SliderContainer } from './style';
+import Swiper from 'swiper';
+
+function Slider (props) {
+  const [ sliderSwiper, setSliderSwiper] = useState(null)
+  const { bannerList } = props
+
+  useEffect(()=>{
+    if( bannerList.length && !sliderSwiper ){
+      let sliderSwiper = new Swiper(".slider-container", {
+        loop: true,
+        autoplay: {
+          delay : 3000,
+          disableOnInteraction: false
+        },
+        pagination: {el:'.swiper-pagination'}
+      });
+
+      setSliderSwiper(sliderSwiper)
+    }
+  },[bannerList, sliderSwiper])
+  return (
+    <SliderContainer>
+      <div className='slider-container'>
+        <div className='swiper-wrapper'>
+          {
+            bannerList.map( (slider, index) => {
+              return (
+                <div className="swiper-slide" key={index}>
+                  <div className="slider-nav">
+                    <img src={slider.imageUrl} width="100%" height="100%" alt="推荐" />
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className="swiper-pagination"></div>
+      </div>
+      <div className="before"></div>
+    </SliderContainer>
+  )
+}
+
+export default React.memo (Slider);
