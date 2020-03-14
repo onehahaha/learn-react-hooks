5b81b7776a8541fdc5d94c1913f2692e866f2f0a
xiaqixiang <xiaqixiang00@163.com>
2020-03-14 20:08:16 +0800

    数据基础搭建，横向分类列表

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,33 +1,58 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Slider from '../../components/slider/index';
+import { connect } from 'react-redux';
+import * as actionTypes from './store/actionCreators'
 import RecommendList from '../../components/list/index';
 import { Content } from './style'
 import Scroll from '../../baseUI/scroll/index'
+import { forceCheck } from 'react-lazyload';
+import Loading from '../../baseUI//loading/index'
 
-function Recommend (props) {
-  const bannerList = [1,2,3,4].map (item => {
-    return { imageUrl: "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg" }
-  });
+function Recommend(props) {
+  const { bannerList, recommendList, enterLoading } = props
+  const { getBannerDataDispatch, getRecommendListDataDispatch} = props
 
-  const recommendList = [1,2,3,4,5,6,7,8,9,10].map (item => {
-    return {
-      id: 1,
-      picUrl: "https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg",
-      playCount: 17171122,
-      name: "朴树、许巍、李健、郑钧、老狼、赵雷"
+  useEffect(()=>{
+    if(!bannerList.size){
+      getBannerDataDispatch ();
     }
-  });
+    if(!recommendList.size){
+      getRecommendListDataDispatch ();
+    }
+  },[])
+
+  const bannerListJS = bannerList ? bannerList.toJS () : [];
+  const recommendListJS = recommendList ? recommendList.toJS () :[];
 
   return (
     <Content>
-      <Scroll className = "list">
+      <Scroll className="list" onScroll={forceCheck}>
         <div>
-          <Slider bannerList={bannerList} ></Slider>
-          <RecommendList recommendList={recommendList}></RecommendList>
+          <Slider bannerList={bannerListJS} ></Slider>
+          <RecommendList recommendList={recommendListJS}></RecommendList>
         </div>
       </Scroll>
+      {enterLoading ? <Loading></Loading>: null}
     </Content>
   )
 }
 
-export default React.memo (Recommend);
+//映射Redux全局的state到组件的props
+const mapStateToProps = (state) => ({
+  // 不要在这里将数据 toJS
+  // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
+  bannerList: state.getIn (['recommend', 'bannerList']),
+  recommendList: state.getIn(['recommend', 'recommendList']),
+  enterLoading: state.getIn(['recommend', 'enterLoading'])
+})
+// 映射 dispatch 到 props 上
+const mapDispachToProps = (dispatch) => ({
+  getBannerDataDispatch() {
+    dispatch(actionTypes.getBannerList())
+  },
+  getRecommendListDataDispatch() {
+    dispatch(actionTypes.getRecommendList());
+  },
+})
+
+export default connect(mapStateToProps, mapDispachToProps)(React.memo(Recommend));
--- src/components/list/index.js
+++ src/components/list/index.js
@@ -5,7 +5,7 @@ import {
   List
 } from './style';
 import { getCount } from '../../api/utils'
-
+import LazyLoad from 'react-lazyload'
 
 function RecommendList  (props) {
   const { recommendList } = props
@@ -19,7 +19,9 @@ function RecommendList  (props) {
               <ListItem key={item.id + index}>
                 <div className='img_wrapper'>
                   <div className="decorate"></div>
-                  <img src={item.picUrl + "?param=300x300"} width="100%" height="100%" alt="music"/>
+                  <LazyLoad placeholder={<img width="100%" height="100%" src={require('./music.png')} alt="music"></img>}>
+                    <img src={item.picUrl + "?param=300x300"} width="100%" height="100%" alt="music"/>
+                  </LazyLoad>
                   <div className='play_count'>
                     <i className="iconfont play">&#xe885;</i>
                     <span className="count">{getCount (item.playCount)}</span>
