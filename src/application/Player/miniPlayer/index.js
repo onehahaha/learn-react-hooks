47858bf9d0374c2def20ff506756da224d75a6ff
xiaqixiang <xiaqixiang@kuaidihelp.com>
2020-04-12 20:07:22 +0800

    播放界面基础搭建

---
+++ src/application/Player/miniPlayer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {getName} from '../../../api/utils';
+import { MiniPlayerContainer } from './style';
+
+function MiniPlayer (props){
+  const { song } = prosp
+  return(
+    <MiniPlayerContainer>
+
+    </MiniPlayerContainer>
+  )
+}
+
+export default React.memo (MiniPlayer);
---
+++ src/application/Player/miniPlayer/style.js
@@ -0,0 +1,15 @@
+import styled, {keyframes} from'styled-components';
+import style from '../../../assets/global-style';
+
+const rotate = keyframes`
+  0%{
+    transform: rotate (0);
+  }
+  100%{
+    transform: rotate (360deg);
+  }
+`
+
+export const MiniPlayerContainer = styled.div`
+
+`
